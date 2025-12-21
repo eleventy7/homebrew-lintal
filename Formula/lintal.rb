5466a62d1354d533bee1fc98b965d3b58a606b68
Shaun L <534336+eleventy7@users.noreply.github.com>
2025-12-20 19:33:19 -0500

    Update SHA256 checksums for v0.1.1

--- Formula/lintal.rb
+++ Formula/lintal.rb
@@ -10,18 +10,18 @@ class Lintal < Formula
   on_macos do
     on_arm do
       url "https://github.com/eleventy7/lintal/releases/download/v#{version}/lintal-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_AARCH64_DARWIN_SHA256"
+      sha256 "1278aaa33752fddf18b8715357c919df12a125dc1eead1814f5ac95de29b7767"
     end
     on_intel do
       url "https://github.com/eleventy7/lintal/releases/download/v#{version}/lintal-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_X86_64_DARWIN_SHA256"
+      sha256 "d5e0aa3ddf0f2c6a37191b6a6176b84a7e44c0a6c0bebc10765b49d32b4df8f1"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/eleventy7/lintal/releases/download/v#{version}/lintal-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_X86_64_LINUX_SHA256"
+      sha256 "6ecd2ee5348dbd22899f0297c1b153a62b8462bb5ac4282c1edee065aeb78092"
     end
   end
 
