3a5138053da04ff5363d579e5b0dc3b2af761e8b
Shaun Laurens <534336+eleventy7@users.noreply.github.com>
2025-12-21 19:15:30 -0500

    Update for 0.1.3

--- Formula/lintal.rb
+++ Formula/lintal.rb
@@ -4,24 +4,24 @@
 class Lintal < Formula
   desc "A fast Java linter with auto-fix, reading checkstyle.xml configs"
   homepage "https://github.com/eleventy7/lintal"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/eleventy7/lintal/releases/download/v#{version}/lintal-aarch64-apple-darwin.tar.gz"
-      sha256 "2ab16f1aacd53698397ee6af56f234eb5d12bf7e1efa610db0a5e4cd0c2cd0eb"
+      sha256 "018ee5e21f55a7f14dda5af3255c4fe239c28e45f4b2193fcda9242042840bbc"
     end
     on_intel do
       url "https://github.com/eleventy7/lintal/releases/download/v#{version}/lintal-x86_64-apple-darwin.tar.gz"
-      sha256 "31494db33504c25ed197abcb9f6ac5fd0e2efeed440c27065fee107843076f8d"
+      sha256 "5bc7558b5fcb1a5769466ca09e060980c2db8b3f8f865745662aaae196fc339f"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/eleventy7/lintal/releases/download/v#{version}/lintal-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "63ee6f2f572c263ebc0ed948d1f3aca42dfcc818e824b493e2ce83e405da798a"
+      sha256 "9ce1a7963dab451215c0f3f1b8abd4bbecba577a496e170ea87df64ed1e97c11"
     end
   end
 
